2f7d92a55ddc8f6f7e3ec2bd6fc93075dbfc7c63
Andrey Chernov <kepkame@gmail.com>
2022-07-28 10:03:45 +0300

    Added page styles

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,2 +1,44 @@
 /* jshint browser: true */
 "use strict";
+
+
+
+jQuery(document).ready(function($){
+  let btnOpenMenu = $('#site-menu-btn-open');
+  let btnCloseMenu = $('#site-menu-btn-close');
+
+  let menu = $('.site-header__col--menu');
+
+  // Open
+  function openMobileMenu() {
+    $('body').addClass('fixed');
+    menu.addClass('site-header__col--menu-show');
+
+    setTimeout(function() {
+        menu.addClass('site-header__col--menu-open');
+    }, 100);
+  }
+
+  // Close
+  function closeMobileMenu() {
+    $('body').removeClass('fixed');
+    menu.removeClass('site-header__col--menu-open');
+
+    setTimeout(function() {
+      menu.removeClass('site-header__col--menu-show');
+      document.body.removeAttribute('style');
+    }, 560);
+
+    $('body').removeClass('fixed');
+  }
+
+  btnOpenMenu.on('click', function() {
+    openMobileMenu(event);
+  });
+
+  btnCloseMenu.on('click', function() {
+    closeMobileMenu(event);
+  });
+});
+
+
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,18 +1,33 @@
 /* jshint browser: true */
-console.log('Test');
 
-/**
- * No-JS - Adds a no-js class that hides JavaScript dependent site elements.
- */
-var elemsClassNojs = document.querySelectorAll('.no-js');
-if (elemsClassNojs[0] !== undefined) {
-  Array.prototype.forEach.call(elemsClassNojs, function (el) {
-    el.classList.remove('no-js');
-  });
-}
+
+
+
+
 
 // jQuery
 jQuery(document).ready(function($){
+
+  // Define the browser
+  function isIE() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+  }
+  
+  // If the browser is IE9 or IE10, then add the id «ie»
+  if( isIE() == 9 || isIE() == 10 ) {
+    $('body').attr('id', 'ie');
+  }
+
+  /**
+   * No-JS - Adds a no-js class that hides JavaScript dependent site elements.
+   */
+  var elemsClassNojs = $('.no-js');
+  if (elemsClassNojs[0] !== undefined) {
+    elemsClassNojs.removeClass('no-js');
+  }
+
+
 
 });
 // jQuery/
